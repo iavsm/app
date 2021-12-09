77c3a2c8109cdd37628cf3d38798df89339bfdfe
iavsm <vasim_shaikh777@yahoo.in>
2021-12-09 11:47:43 +0530

    Edit and Update Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,26 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to article_path(@article)
+      if @article.save
+        flash[:notice] = 'Article created sucessfully'
+        redirect_to article_path(@article)
+      else 
+        render 'new'
+      end    
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = 'Article updated sucessfully'
+        redirect_to article_path(@article)
+      else 
+        render 'new'
+      end  
   end
 
 end
