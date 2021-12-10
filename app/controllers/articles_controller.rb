e474866335e74422cb0af9fab3947a198bf8e084
iavsm <vasim_shaikh777@yahoo.in>
2021-12-10 13:21:16 +0530

    code refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def show
-      @article = Article.find(params[:id])
+     # @article = Article.find(params[:id])
   end
 
   def index
@@ -9,11 +11,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    
+    @article = Article.new
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(get_params)
       if @article.save
         flash[:notice] = 'Article created sucessfully'
         redirect_to article_path(@article)
@@ -23,17 +25,33 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+    #@article = Article.find(params[:id])
+      if @article.update(get_params)
         flash[:notice] = 'Article updated sucessfully'
         redirect_to article_path(@article)
       else 
         render 'new'
       end  
   end
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def get_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
